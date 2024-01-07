85507fff41ffefa59b880bd0004a9abed452dc84
Tetsu Yamakawa <68310167+tecchu11@users.noreply.github.com>
2024-01-07 17:45:53 +0900

    feature/use buildSrc (#12)
    * update java to 21
    
    * use buildSrc and versioncatalog

---
+++ buildSrc/src/main/kotlin/com/example/ext/VersionCatalogExt.kt
@@ -0,0 +1,21 @@
+package com.example.ext
+
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
+import org.gradle.api.artifacts.VersionCatalog
+import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.artifacts.dsl.DependencyHandler
+import org.gradle.kotlin.dsl.getByType
+
+/**
+ * Project.libs get [VersionCatalog] defined in settings.gradle.kts.
+ */
+internal val Project.libs: VersionCatalog
+    get() = extensions
+        .getByType<VersionCatalogsExtension>()
+        .named("libs")
+
+/**
+ * Versioncatalog.of is extension function to get libraries with given alias from [VersionCatalog].
+ */
+internal fun VersionCatalog.of(alias: String) = this.findLibrary(alias).get()
